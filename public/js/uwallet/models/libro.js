c98d1f34b0a6efe12259b86cc80b1d945b1c2dcb
jonatan360 <japarrat@unal.edu.co>
2017-10-06 16:37:38 -0500

    plantilla inicial hecha en clase

---
+++ public/js/uwallet/models/libro.js
@@ -0,0 +1,12 @@
+/*var Libro = Backbone.Model.extend({
+	initialize: function(){
+		console.log('Se ha creado una nueva instancia');
+
+		this.on('change', function({
+			console.log('El modelo ha cambiado');
+		}));
+	},
+	defaults: {
+		autor: 'Desconocido',
+	}
+});*/
