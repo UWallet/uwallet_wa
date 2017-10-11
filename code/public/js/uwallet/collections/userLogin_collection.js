8ccad6ba50cefa2b618e8e39c8a121c5ca62edeb
Jonatan <japarrat@unal.edu.co>
2017-10-11 15:19:55 -0500

    correcion en una colleccion

--- code/public/js/uwallet/collections/userLogin_collection.js
+++ code/public/js/uwallet/collections/userLogin_collection.js
@@ -1,7 +1,7 @@
 var app = app || {};
 
 var Userlogin_collection = Backbone.Collection.extend({
-  Model: app.Userreg_model,
+  Model: app.Userlogin_model,
   url: "http://192.168.99.101:4000/users/login"
 });
 
